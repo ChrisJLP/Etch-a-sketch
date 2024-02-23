ec5a257557ced692480db1a50648922f9c68f64f
Chris Pring <chrisjlpring@outlook.com>
2024-02-23 13:16:30 +0000

    Add functionality for the grid to change size based on number of squares

--- javascript.js
+++ javascript.js
@@ -3,12 +3,12 @@ let gridY = 8;
 
 const container = document.querySelector('.container');
 
-for (let i = 0; i < gridY; i++) {
+for (let i = 0; i < gridX; i++) {
     const aRow = document.createElement('div');
     aRow.classList.add('row');
     container.appendChild(aRow);
 
-    for (let j = 0; j < gridX; j++) {
+    for (let j = 0; j < gridY; j++) {
         const aSquare = document.createElement('div');
         aSquare.classList.add('square');
         aRow.appendChild(aSquare);
