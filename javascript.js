f0033bccb24ee7d0cd76d3a6eae8f469dc328b3c
Chris Pring <chrisjlpring@outlook.com>
2024-02-23 14:26:02 +0000

    Add logic to new grid button in javascript.js, fix styling when page is fullscreen

--- javascript.js
+++ javascript.js
@@ -3,23 +3,51 @@ let gridY = 8;
 
 const container = document.querySelector('.container');
 
-for (let i = 0; i < gridX; i++) {
-    const aRow = document.createElement('div');
-    aRow.classList.add('row');
-    container.appendChild(aRow);
+makeGrid();
 
-    for (let j = 0; j < gridY; j++) {
-        const aSquare = document.createElement('div');
-        aSquare.classList.add('square');
-        aRow.appendChild(aSquare);
+function makeGrid () {
+    deleteGrid();
+    for (let i = 0; i < gridX; i++) {
+        const aRow = document.createElement('div');
+        aRow.classList.add('row');
+        container.appendChild(aRow);
+    
+        for (let j = 0; j < gridY; j++) {
+            const aSquare = document.createElement('div');
+            aSquare.classList.add('square');
+            aRow.appendChild(aSquare);
+        }
     }
+    makeHoverFunctionality();
 }
 
-const squares = document.querySelectorAll('.square');
-console.log(squares);
-
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'green';
+function makeHoverFunctionality() {
+    const squares = document.querySelectorAll('.square');
+   
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'green';
+        })
     })
-})
+}
+
+const newGrid = document.querySelector('.newGridButton');
+newGrid.addEventListener('click', () => {
+    console.log("hi");
+    let newSize = prompt("What size grid would you like? Max 100");
+    if ((isNaN(+newSize)) || +newSize > 100) {
+        while ((isNaN(+newSize)) || +newSize > 100) {
+            newSize = prompt("Please enter a valid number");
+        }
+    } 
+    gridX = newSize;
+    gridY = newSize;
+    makeGrid();
+});
+
+function deleteGrid() {
+    const rows = document.querySelectorAll('.row');
+    rows.forEach((row) => {
+        row.remove();
+    })
+};
